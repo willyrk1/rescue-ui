2c7c830b50bc02f2fe087e180982d66ed8589fde
Bill Knight <bknight@masonite.com>
2019-11-24 13:50:36 -0500

    Add/align stuff in banner

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
-
+import hero from '../assets/images/hero.png'
 
 class Hero extends React.Component {
   render() {
     return (
-      <section className="hero__background">
+      <section className='banner'>
+        <img src={hero} />
+        <div>
+          <h1>
+            CUDDLE<br/>
+            <span style={{color: '#B2E63B'}}>PARTY</span>
+          </h1>
+          <hr/>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed venenatis, mi id imperdiet rutrum, urna ante faucibus ex, non vulputate
+            enim est gravida enim. Suspendisse eu tortor dui.
+          </p>
+        </div>
       </section>
     );
   };
