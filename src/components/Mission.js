c5de669cc3fd38ba4542019598d1f06e04a0b86e
Bill Knight <bknight@masonite.com>
2019-12-10 22:58:33 -0500

    Hooksify the Mission.  Merge BoardMember into Mission.

--- src/components/Mission.js
+++ src/components/Mission.js
@@ -6,61 +6,94 @@
  * author: Steven Pothoven (steven@pothoven.net)
  ********************************************************************************/
 
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
+import classNames from 'classnames/bind'
 import StFrancisRescue from '../apis/StFrancisRescue';
 import Layout from './Layout'
-import BoardMember from './BoardMember';
+import {PROTOCOL, HOSTNAME} from '../config/StFrancisRescue';
+import styles from './Mission.module.scss';
 
-class Mission extends Component {
-  constructor(props) {
-    super(props);
+const cx = classNames.bind(styles)
 
-    this.state = { data : {}};
+const Mission = () => {
+  const [boardMembers, setBoardMembers] = useState()
 
-    StFrancisRescue.getBoardMembers()
-      .then(data => this.loadBoardMembers(data))
-      .catch(error => console.error(error));
-  }
-
-  loadBoardMembers(data) {
-    this.setState({data : data.data});
-  }
-
-  renderBoardMembers(boardMembers) {
-    const b = [];
-    if (boardMembers) {
-      boardMembers.forEach(boardMember => {
-        b.push(<BoardMember boardMember={boardMember} key={`board-member-${boardMember.id}`} />);
-      });
+  useEffect(() => {
+    const loadBoardMembers = async () => {
+      const { data } = await StFrancisRescue.getBoardMembers()
+      setBoardMembers(data)
     }
-    return <div>{b}</div>;
-  }
+    loadBoardMembers()
+  }, [])
 
-  render() {
-    const { presidents, vicePresidents, treasurers, secreteries, directors, adminStaff } = this.state.data
-      
-    return (
-      <Layout>
-        <div id="missions">
-          <h3>Our mission at St. Francis</h3>
-          <p>St. Francis Society Animal Rescue is an all volunteer, non-profit 501-c-3 animal rescue organization dedicated to saving the lives of sick, injured, and stray domestic animals as well as spaying/neutering and medical services for those animals. We seek to place animals in a loving foster or permanent home after recovery and we DO NOT euthanize unless terminal illness necessitates such a decision. All of our animals are spayed/neutered, tested for leukemia/FIV, wormed and vaccinated&nbsp; before they are adopted.</p>
-
-          <p><strong><span style={{fontSize: "medium", fontFamily: "comic sans ms,sans-serif"}}>Why We Chose the Name St. Francis</span></strong></p>
-              
-          <p>Poorly dressed in rough brown cloth, a little thin man felt compassion for the smallest of God's creatures. He brought safety to worms he found in the middle of the road by gently picking them up and placing them on the side of the road. St. Francis of Assissi chose to live in poverty, while rejoicing in the world's natural beauty. Joyfully praising "Brother Sun, Sister Moon, Brother Fire and Sister Water" thru his poetry. Even more, he loved living creatures; teaching that all should be generous to animals and birds. Throughout his travels, St. Francis spoke of God's great love and care for his companion animals. During a long winter travel, he brought honey to the bees, exchanged his cloak to a butcher in return for two lambs - to save them from slaughter. He urged people to throw out grain for the birds, and was seen throwing freshly caught fish back in the water. He was loved and followed by many pet animals; lambs, rabbits, and pheasants, among them. Today, we celebrate the "Feast of St. Francis" on October 4th. Across the world, a Blessing of the Animals is held on this day.</p>
-
-          <h3>Board of Directors</h3>
-          {this.renderBoardMembers(presidents)}
-          {this.renderBoardMembers(vicePresidents)}
-          {this.renderBoardMembers(treasurers)}
-          {this.renderBoardMembers(secreteries)}
-          {this.renderBoardMembers(directors)}
-          {this.renderBoardMembers(adminStaff)}
-
+  return (
+    <Layout>
+      <div className={cx("mission-row")}>
+        <div class={cx('side')}>
+          <div style={{width: '10rem', height: '10rem', border: '1px solid red'}}/>
         </div>
-      </Layout>
-    )
-  }
+        <div class={cx('main')}>
+          <h1>Our Mission</h1>
+          <p>
+            St. Francis Society Animal Rescue is an all volunteer, non-profit 501-c-3 animal
+            rescue organization dedicated to saving the lives of sick, injured, and stray
+            domestic animals as well as spaying/neutering and medical services for those
+            animals. We seek to place animals in a loving foster or permanent home after
+            recovery and we DO NOT euthanize unless terminal illness necessitates such a
+            decision. All of our animals are spayed/neutered, tested for leukemia/FIV, wormed
+            and vaccinated before they are adopted.
+          </p>
+
+          <h2>Why did we choose St. Francis?</h2>
+              
+          <p>
+            Poorly dressed in rough brown cloth, a little thin man felt compassion for the
+            smallest of God's creatures. He brought safety to worms he found in the middle of
+            the road by gently picking them up and placing them on the side of the road. St.
+            Francis of Assissi chose to live in poverty, while rejoicing in the world's natural
+            beauty. Joyfully praising "Brother Sun, Sister Moon, Brother Fire and Sister
+            Water" thru his poetry. Even more, he loved living creatures; teaching that all
+            should be generous to animals and birds. Throughout his travels, St. Francis spoke
+            of God's great love and care for his companion animals. During a long winter
+            travel, he brought honey to the bees, exchanged his cloak to a butcher in return
+            for two lambs - to save them from slaughter. He urged people to throw out grain for
+            the birds, and was seen throwing freshly caught fish back in the water. He was
+            loved and followed by many pet animals; lambs, rabbits, and pheasants, among them.
+            Today, we celebrate the "Feast of St. Francis" on October 4th. Across the world, a
+            Blessing of the Animals is held on this day.
+          </p>
+
+          <h2>Meet our board of Directors</h2>
+        </div>
+      </div>
+      {boardMembers && [
+        'presidents',
+        'vicePresidents',
+        'treasurers',
+        'secreteries',
+        'directors',
+        'adminStaff'
+      ].map(boardType => boardMembers[boardType].map(({
+        picture, volunteer: { name }, board_member_title: { name: title }, role, bio
+      }) =>
+        <div className={cx('mission-row boardMember')}>
+          <div class={cx('side')}>
+            <img
+              src={picture
+                ? `${PROTOCOL}://${HOSTNAME}${picture}`
+                : "/no_picture.jpg"
+              }
+            />
+          </div>
+          <div class={cx('main')}>
+            <h3>{name.split(' ')[0]}-{title}</h3>
+            <h4>{role}</h4>
+            <p dangerouslySetInnerHTML={{__html: bio }} />
+          </div>
+        </div>
+      ))}
+    </Layout>
+  )
 }
 
 export default Mission;
