d0e7c5c0dc4687d64a876a6586e68efc7130012e
Bill Knight <bknight@masonite.com>
2019-12-17 22:18:39 -0500

    Try https

--- src/config/StFrancisRescue.js
+++ src/config/StFrancisRescue.js
@@ -1,6 +1,6 @@
 /* jshint esversion: 6 */
 
-export const PROTOCOL = "http";
+export const PROTOCOL = "https";
 //export const HOSTNAME = "0.0.0.0:8080";
 export const HOSTNAME = "stfrancisrescue.org";
 export const ROOT_URL = "/";
