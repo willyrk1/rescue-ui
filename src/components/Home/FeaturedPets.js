bf57e08bdb26db42815747ac182000f5b0000650
Bill Knight <bknight@masonite.com>
2019-12-24 10:52:48 -0500

    Snazzy width deriver in FeaturedPets for new design

--- src/components/Home/FeaturedPets.js
+++ src/components/Home/FeaturedPets.js
@@ -22,26 +22,37 @@ const FeaturedPets = () => {
     loadPets()
   }, [])
 
+  const canvasHeight = 360 // image max-height
+  const canvasWidth = 1020 // total width (1200) minus 2 * 8rem padding minus p margin
+
   const pet = pets && pets[Math.floor(Math.random() * pets.length)]
-  const { public_filename: petImage = null } =
-    (pet && pet.images.find(({ primary }) => primary)) || {}
+  const {
+    public_filename: petImage,
+    height,
+    width,
+  } = (pet && pet.images.find(({ primary }) => primary)) || {}
+
+  const realImageWidth = width * (canvasHeight / height)
+  const textWidth = pet && (pet.story.length * 0.65)
+  const padding = (canvasWidth - realImageWidth - textWidth) / 2
 
   return (
     <div className={cx('featured-pets')}>
       <h1>Featured Pet</h1>
       <hr/>
       {pet &&
-        <div className={cx('pet-outer')}>
-          <h2>{pet.name}</h2>
-          <div className={cx('pet')}>
-            {console.log(pet)}
-            <img src={`${PROTOCOL}://${HOSTNAME}${petImage}`} />
-            <p>{pet.story}</p>
-            <div className={cx("btn-container")}>
-              <a className={cx("btn btn--accent")} href="#">View All Cats</a>
-              <a className={cx("btn btn--accent")} href="#">View All Dogs</a>
-            </div>
+        <div className={cx('pet-outer')} style={{ padding: `0 ${padding}px`}}>
+          <div>
+            <h2>{pet.name}</h2>
+              <div className={cx('pet')}>
+                <img src={`${PROTOCOL}://${HOSTNAME}${petImage}`} />
+              <p>{pet.story}</p>
           </div>
+          <div className={cx("btn-container")}>
+            <a className={cx("btn btn--accent")} href="#">View All Cats</a>
+            <a className={cx("btn btn--accent")} href="#">View All Dogs</a>
+          </div>
+        </div>
         </div>
       }
     </div>
