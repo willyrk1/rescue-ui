46ee15f2ad84eddf559bb6809797291c1f96e40a
Bill Knight <bknight@masonite.com>
2019-12-24 19:13:12 -0500

    Branch with multiple featured

--- src/components/Home/FeaturedPets.js
+++ src/components/Home/FeaturedPets.js
@@ -22,25 +22,37 @@ const FeaturedPets = () => {
     loadPets()
   }, [])
 
-  const pet = pets && pets[Math.floor(Math.random() * pets.length)]
-  const { public_filename: petImage = null } =
-    (pet && pet.images.find(({ primary }) => primary)) || {}
+  const randomPets = pets && (() => {
+    const randomSet = new Set()
+    while (randomSet.size < 3)
+    {
+      randomSet.add(Math.floor(Math.random() * pets.length))
+    }
+    return [...randomSet].map(index => {
+      const pet = pets[index]
+      const { public_filename: petImage = null } =
+        (pet && pet.images.find(({ primary }) => primary)) || {}
+      return { ...pet, petImage }
+    })
+  })()
 
   return (
     <div className={cx('featured-pets')}>
-      <h1>Featured Pet</h1>
+      <h1>Featured Pets</h1>
       <hr/>
-      {pet &&
+      {randomPets &&
         <div className={cx('pet-outer')}>
-          <h2>{pet.name}</h2>
-          <div className={cx('pet')}>
-            {console.log(pet)}
-            <img src={`${PROTOCOL}://${HOSTNAME}${petImage}`} />
-            <p>{pet.story}</p>
-            <div className={cx("btn-container")}>
-              <a className={cx("btn btn--accent")} href="#">View All Cats</a>
-              <a className={cx("btn btn--accent")} href="#">View All Dogs</a>
-            </div>
+          <div className={cx('pet-list')}>
+            {randomPets.map(({ name, petImage }) =>
+              <div className={cx('pet')}>
+                <h2>{name}</h2>
+                <img src={`${PROTOCOL}://${HOSTNAME}${petImage}`} />
+              </div>
+            )}
+          </div>
+          <div className={cx("btn-container")}>
+            <a className={cx("btn btn--accent")} href="#">View All Cats</a>
+            <a className={cx("btn btn--accent")} href="#">View All Dogs</a>
           </div>
         </div>
       }
