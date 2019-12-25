b0accf6963e694186654112bb4c2c680273885ea
Bill Knight <bknight@masonite.com>
2019-12-24 21:15:00 -0500

    This FeaturedPets version has some promise.

--- src/components/Home/FeaturedPets.js
+++ src/components/Home/FeaturedPets.js
@@ -22,42 +22,62 @@ const FeaturedPets = () => {
     loadPets()
   }, [])
 
-  const canvasHeight = 300 // image max-height
-  const canvasWidth = 986
-
   const pet = pets && pets[Math.floor(Math.random() * pets.length)]
   const {
     public_filename: petImage,
-    height,
-    width,
   } = (pet && pet.images.find(({ primary }) => primary)) || {}
-
-  const realImageWidth = width * (canvasHeight / height)
-  const textWidth = pet && (pet.story.length * 0.75)
-  const padding = (canvasWidth - realImageWidth - textWidth) / 2
 
   return (
     <div className={cx('featured-pets')}>
-      <div>
-        <h1>Featured Pet</h1>
-        <hr/>
-        {pet &&
-          <div className={cx('pet-outer')} style={{ padding: `0 ${padding}px`}}>
-        {console.log(pet)}
-            <div>
+      <h1>Featured Pet</h1>
+      <hr/>
+      {pet &&
+        <>
+{console.log(pet)}
+          <div className={cx('pet-outer')}>
+            <div className={cx('pet-info')}>
               <h2>{pet.name}</h2>
-                <div className={cx('pet')}>
-                  <img src={`${PROTOCOL}://${HOSTNAME}${petImage}`} />
-                <p>{pet.story}</p>
+              <ul>
+                <li>
+                  <label htmlFor='breed'>Breed</label>
+                  <span id='breed'>{pet.dominant_breed.name}</span>
+                </li>
+                <li>
+                  <label htmlFor='gender'>Gender</label>
+                  <span id='gender'>{pet.sex}</span>
+                </li>
+                <li>
+                  <label htmlFor='color'>Color</label>
+                  <span id='color'>{pet.animal_color.name}</span>
+                </li>
+                <li>
+                  <label htmlFor='dob'>Date of birth</label>
+                  <span id='dob'>{pet.date_of_birth}</span>
+                </li>
+                <li>
+                  <label htmlFor='goodWithCats'>Good with cats</label>
+                  <span id='goodWithCats'>{pet.good_with_cats}</span>
+                </li>
+                <li>
+                  <label htmlFor='goodWithDogs'>Good with dogs</label>
+                  <span id='goodWithDogs'>{pet.good_with_dogs}</span>
+                </li>
+                <li>
+                  <label htmlFor='goodWithChildren'>Good with children</label>
+                  <span id='goodWithChildren'>{pet.good_with_children}</span>
+                </li>
+              </ul>
             </div>
-            <div className={cx("btn-container")}>
-              <a className={cx("btn btn--accent")} href="#">View All Cats</a>
-              <a className={cx("btn btn--accent")} href="#">View All Dogs</a>
+            <div className={cx('pet-image')}>
+              <img src={`${PROTOCOL}://${HOSTNAME}${petImage}`} />
             </div>
           </div>
+          <div className={cx("btn-container")}>
+            <a className={cx("btn btn--accent")} href="#">View All Cats</a>
+            <a className={cx("btn btn--accent")} href="#">View All Dogs</a>
           </div>
-        }
-      </div>
+        </>
+      }
     </div>
   )
 }
