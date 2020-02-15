83c87421e8c7bd709c82448de676283895d89d68
Bill Knight <BKnight@Masonite.com>
2020-02-15 12:53:03 -0500

    Start an attempt at full-screen modal gallery for animal detail

--- src/components/AnimalDetails.js
+++ src/components/AnimalDetails.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import classNames from 'classnames/bind'
+import Popup from 'reactjs-popup'
 import {PROTOCOL, HOSTNAME} from '../config/StFrancisRescue';
 import StandardLayout from './StandardLayout'
 import Scroller from './Scroller'
@@ -9,9 +10,28 @@ const cx = classNames.bind(styles)
 
 const AnimalDetails = ({ location: { state: { pet }}}) => {
   const sharedState = useState(0)
-  const components = pet.images.map(({ public_filename }) => ({
+
+  const modalComponents = pet.images.map(({ public_filename }) => ({
     key: public_filename,
     component: <img src={`${PROTOCOL}://${HOSTNAME}${public_filename}`} />
+  }))
+  const popupComponents = modalComponents.map(({ component, ...rest }) => ({
+    ...rest,
+    component:
+      <Popup
+        modal
+        closeOnDocumentClick
+        trigger={<button className={cx('popup')}>{component}</button>}
+      >
+        <div className={cx('gallery', 'gallery--popup')}>
+          <Scroller
+            components={modalComponents}
+            styles={styles}
+            scrollRems={42}
+            state={sharedState}
+          />
+        </div>
+      </Popup>
   }))
 
   return (
@@ -19,17 +39,17 @@ const AnimalDetails = ({ location: { state: { pet }}}) => {
       <div className={cx('animal-details')}>
         <h1>{pet.name}</h1>
         <hr/>
-        <div className={cx('gallery--big')}>
+        <div className={cx('gallery', 'gallery--big')}>
           <Scroller
-            components={components}
+            components={popupComponents}
             styles={styles}
             scrollRems={42}
             state={sharedState}
           />
         </div>
-        <div className={cx('gallery--thumbnail')}>
+        <div className={cx('gallery', 'gallery--thumbnail')}>
           <Scroller
-            components={components}
+            components={popupComponents}
             styles={styles}
             scrollRems={10}
             state={sharedState}
