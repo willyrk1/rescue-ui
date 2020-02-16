824a3aaeeacd4cbd61959f39955bf9ed328496ae
Bill Knight <BKnight@Masonite.com>
2020-02-16 11:38:51 -0500

    Some local environment support

--- src/config/StFrancisRescue.js
+++ src/config/StFrancisRescue.js
@@ -1,6 +1,15 @@
 /* jshint esversion: 6 */
 
+const env = process.env.REACT_APP_LOCAL || 'dev'
+
+const { hostName } = {
+  dev: {},
+  stage: {
+    hostName: 'staging.stfrancisrescue.org'
+  },
+}[env]
+
 export const PROTOCOL = "https";
 //export const HOSTNAME = "0.0.0.0:8080";
-export const HOSTNAME = "stfrancisrescue.org";
+export const HOSTNAME = hostName || "staging.stfrancisrescue.org";
 export const ROOT_URL = "/";
