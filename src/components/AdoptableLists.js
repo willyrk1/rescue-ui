cf99393a80c87a39b670a1ed0dafdcc4db817442
Bill Knight <BKnight@Masonite.com>
2020-02-18 22:53:14 -0500

    Continue AnimalSearch with simpler useReducer approach

--- src/components/AdoptableLists.js
+++ src/components/AdoptableLists.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useReducer } from 'react'
+import React, { useEffect, useReducer } from 'react'
 import classNames from 'classnames/bind'
 import StFrancisRescue from '../apis/StFrancisRescue'
 import StandardLayout from './StandardLayout'
@@ -11,28 +11,44 @@ const cx = classNames.bind(styles)
 const AdoptableList = ({ getPets, lists, children }) => {
   const getUniqueOptions = (pets, animalProperty) => [...new Set(
     lists.reduce((acc, { property }) => {
-      acc.push(...pets[property].map(({ [animalProperty]: value }) => value))
+      acc.push(...pets[property].map(pet =>
+        typeof animalProperty === 'function' ? animalProperty(pet) : pet[animalProperty]
+      ))
       return acc
     }, [])
   )].map(value => ({ value, label: value }))
 
-  const [state, dispatch] = useReducer((state, pets) => ({
-    pets,
-    genderChoices: getUniqueOptions(pets, 'sex'),
-  }), { pets: {} })
-
-  const [searchName, setSearchName] = useState()
-  const [searchGender, setSearchGender] = useState()
+  const [state, dispatch] = useReducer((state, { type, pets, ...params }) => {
+    switch (type) {
+      case 'INITIALIZE':
+        return {
+          ...state,
+          pets,
+          genderChoices: getUniqueOptions(pets, 'sex'),
+          breedChoices: getUniqueOptions(pets, ({ dominant_breed }) => dominant_breed.name),
+          colorChoices: getUniqueOptions(pets, ({ animal_color }) => animal_color.name),
+          ageChoices: [
+            { value: '<6m', label: 'Under 6 months' },
+            { value: '6m-1y', label: '6 months to 1 year' },
+            { value: '>1y', label: 'over 1 year' },
+          ],
+        }
+      case 'SET':
+        return { ...state, ...params }
+      default:
+        break
+    }
+  }, { pets: {} })
 
   useEffect(() => {
     const loadPets = async () => {
-      const petData = getPets(StFrancisRescue)
-      dispatch((await petData).data)
+      const { data: pets } = await getPets(StFrancisRescue)
+      dispatch({ type: 'INITIALIZE', pets})
     }
     loadPets()
   }, [getPets])
 
-  const { pets, genderChoices } = state
+  const { pets, searchName, searchGender, searchBreed, searchColor, searchAge } = state
 
   return (
     <StandardLayout>
@@ -41,17 +57,20 @@ const AdoptableList = ({ getPets, lists, children }) => {
       {pets && lists.map(({ property, title }) =>
         pets[property] && pets[property].length &&
           <div className={cx('adoptable-list')} key={property}>
-            <AnimalSearch
-              {...{
-                cx,
-                searchName, setSearchName,
-                genderChoices, searchGender, setSearchGender,
-              }}
-            />
+            <AnimalSearch {...{ cx, state, dispatch }} />
             <h2>{title}</h2>
             <div>
               {pets[property]
                 // .filter(({ youtube_url }) => youtube_url)
+                .filter(({
+                  name, sex,
+                  dominant_breed: { name: breed },
+                  animal_color: { name: color },
+                }) => name.toUpperCase().includes((searchName || '').toUpperCase())
+                  && (!searchGender || sex === searchGender.value)
+                  && (!searchBreed || breed === searchBreed.value)
+                  && (!searchColor || color === searchColor.value)
+                )
                 .slice(0, 20)
                 .map(pet => <AnimalCard pet={pet} key={pet.id} />)
               }
--- src/components/AnimalSearch.js
+++ src/components/AnimalSearch.js
@@ -3,38 +3,73 @@ import Popup from 'reactjs-popup'
 import StandardForm from './StandardForm'
 
 const AnimalSearch = ({
-  cx,
-  searchName, setSearchName,
-  genderChoices, searchGender, setSearchGender,
-}) =>
-  <Popup
-    modal
-    closeOnDocumentClick
-    trigger={<button className={cx('btn', 'search')}>Search</button>}
-  >
-    <div className={cx('search-content')}>
-      <StandardForm>
-        <ul>
-          <li>
-            <label htmlFor='name'>Name</label>
-            <input
-              type='text'
-              id='name'
-              value={searchName}
-              onChange={({ target }) => setSearchName(target.value)}
-            />
-          </li>
-          <li>
-            <label htmlFor='gender'>Gender</label>
-            <StandardForm.Select
-              id='gender'
-              options={genderChoices}
-              value={searchGender}
-              onChange={setSearchGender} />
-          </li>
-        </ul>
-      </StandardForm>
-    </div>
-  </Popup>
+  cx, dispatch,
+  state: {
+    searchName,
+    genderChoices, searchGender,
+    breedChoices, searchBreed,
+    colorChoices, searchColor,
+    ageChoices, searchAge,
+  },
+}) => {
+  const setInput = property => ({ target }) => dispatch({ type: 'SET', [property]: target.value })
+  const setSelect = property => value => dispatch({ type: 'SET', [property]: value })
+
+  return (
+    <Popup
+      modal
+      closeOnDocumentClick
+      trigger={<button className={cx('btn', 'search')}>Search</button>}
+    >
+      <div className={cx('search-content')}>
+        <StandardForm>
+          <ul>
+            <li>
+              <label htmlFor='name'>Name</label>
+              <input
+                type='text'
+                id='name'
+                value={searchName}
+                onChange={setInput('searchName')}
+              />
+            </li>
+            <li>
+              <label htmlFor='gender'>Gender</label>
+              <StandardForm.Select
+                id='gender'
+                options={genderChoices}
+                value={searchGender}
+                onChange={setSelect('searchGender')} />
+            </li>
+            <li>
+              <label htmlFor='breed'>Breed</label>
+              <StandardForm.Select
+                id='breed'
+                options={breedChoices}
+                value={searchBreed}
+                onChange={setSelect('searchBreed')} />
+            </li>
+            <li>
+              <label htmlFor='color'>Color</label>
+              <StandardForm.Select
+                id='color'
+                options={colorChoices}
+                value={searchColor}
+                onChange={setSelect('searchColor')} />
+            </li>
+            <li>
+              <label htmlFor='age'>Age</label>
+              <StandardForm.Select
+                id='age'
+                options={ageChoices}
+                value={searchAge}
+                onChange={setSelect('searchAge')} />
+            </li>
+          </ul>
+        </StandardForm>
+      </div>
+    </Popup>
+  )
+}
 
 export default AnimalSearch
