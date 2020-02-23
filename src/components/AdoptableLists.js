08b2a940ca46506b71ba89f7be5c126bd9340140
Bill Knight <BKnight@Masonite.com>
2020-02-23 09:41:07 -0500

    This is getting weird...

--- src/components/AdoptableLists.js
+++ src/components/AdoptableLists.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useReducer, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import classNames from 'classnames/bind'
 import StFrancisRescue from '../apis/StFrancisRescue'
 import StandardLayout from './StandardLayout'
@@ -8,86 +8,101 @@ import styles from './AdoptableLists.module.scss'
 
 const cx = classNames.bind(styles)
 
+const getUniqueOptions = animalProperty => (pets, lists) => [...new Set(
+  lists.reduce((acc, { property }) => {
+    acc.push(...pets[property].map(pet =>
+      typeof animalProperty === 'function' ? animalProperty(pet) : pet[animalProperty]
+    ))
+    return acc
+  }, [])
+)].map(value => ({ value, label: value }))
+
+const getDaysAgo = days => new Date() - days * 1000 * 60 * 60 * 24
+
 const AdoptableList = ({ getPets, lists, children }) => {
-  const getUniqueOptions = (pets, animalProperty) => [...new Set(
-    lists.reduce((acc, { property }) => {
-      acc.push(...pets[property].map(pet =>
-        typeof animalProperty === 'function' ? animalProperty(pet) : pet[animalProperty]
-      ))
-      return acc
-    }, [])
-  )].map(value => ({ value, label: value }))
+  const [pets, setPets] = useState([])
 
-  const getDaysAgo = days => new Date() - days * 1000 * 60 * 60 * 24
-
-  const [state, dispatch] = useReducer((state, { type, pets, ...params }) => {
-    switch (type) {
-      case 'INITIALIZE':
-        return {
-          ...state,
-          pets,
-          genderChoices: getUniqueOptions(pets, 'sex'),
-          breedChoices: getUniqueOptions(pets, ({ dominant_breed }) => dominant_breed.name),
-          colorChoices: getUniqueOptions(pets, ({ animal_color }) => animal_color.name),
-          ageChoices: [
-            {
-              value: '<6m',
-              label: 'Under 6 months',
-              min: getDaysAgo(6 * 30),
-            },
-            {
-              value: '6m-1y',
-              label: '6 months to 1 year',
-              max: getDaysAgo(6 * 30),
-              min: getDaysAgo(365),
-            },
-            {
-              value: '>1y',
-              label: 'over 1 year',
-              max: getDaysAgo(365),
-            },
-          ],
-          furLengthChoices: getUniqueOptions(pets, 'fur_length'),
-          declawedChoices: getUniqueOptions(pets, 'declawed'),
-          goodWithChildrenChoices: getUniqueOptions(pets, 'good_with_children'),
-          goodWithCatsChoices: getUniqueOptions(pets, 'good_with_cats'),
-          goodWithDogsChoices: getUniqueOptions(pets, 'good_with_dogs'),
-        }
-      case 'SET':
-        return { ...state, ...params }
-      default:
-        break
-    }
-  }, {})
+  const [criteria, setCriteria] = useState([{
+    inputType: 'text',
+    label: 'Name',
+  }, {
+    inputType: 'select',
+    label: 'Gender',
+    choiceFn: getUniqueOptions('sex'),
+  }, {
+    inputType: 'select',
+    label: 'Breed',
+    choiceFn: getUniqueOptions(({ dominant_breed }) => dominant_breed.name),
+  }, {
+    inputType: 'select',
+    label: 'Color',
+    choiceFn: getUniqueOptions(({ animal_color }) => animal_color.name),
+  }, {
+    inputType: 'select',
+    label: 'Age',
+    choiceFn: () => [
+      {
+        value: '<6m',
+        label: 'Under 6 months',
+        min: getDaysAgo(6 * 30),
+      },
+      {
+        value: '6m-1y',
+        label: '6 months to 1 year',
+        max: getDaysAgo(6 * 30),
+        min: getDaysAgo(365),
+      },
+      {
+        value: '>1y',
+        label: 'over 1 year',
+        max: getDaysAgo(365),
+      },
+    ],
+  }, {
+    inputType: 'select',
+    label: 'Fur Length',
+    choiceFn: getUniqueOptions('fur_length'),
+  }, {
+    inputType: 'select',
+    label: 'Declawed',
+    choiceFn: getUniqueOptions('declawed'),
+  }, {
+    inputType: 'select',
+    label: 'Good with Children',
+    choiceFn: getUniqueOptions('good_with_children'),
+  }, {
+    inputType: 'select',
+    label: 'Good with Cats',
+    choiceFn: getUniqueOptions('good_with_cats'),
+  }, {
+    inputType: 'select',
+    label: 'Good with Dogs',
+    choiceFn: getUniqueOptions('good_with_dogs'),
+  }])
 
   useEffect(() => {
     const loadPets = async () => {
       const { data: pets } = await getPets(StFrancisRescue)
-      dispatch({ type: 'INITIALIZE', pets})
+      setPets(pets)
+      setCriteria(criteria => criteria.map(criterion => ({
+        ...criterion,
+        ...criterion.choiceFn ? { choices: criterion.choiceFn(pets, lists) } : null,
+      })))
     }
     loadPets()
-  }, [getPets])
-
-  const {
-    pets, searchName, searchGender, searchBreed, searchColor, searchAge, searchFurLength,
-    searchDeclawed, searchGoodWithChildren, searchGoodWithCats, searchGoodWithDogs,
-  } = state
-
-  const doingSearch = searchName || searchGender || searchBreed || searchColor ||
-    searchFurLength || searchDeclawed || searchGoodWithChildren || searchGoodWithCats ||
-    searchGoodWithDogs || searchAge
+  }, [getPets, lists])
 
   const [pageNums, setPageNums] = useState({})
 
   // If there are searches, then filter.  Otherwise, slice the given page.
   const processList = (collection, pageNum) => {
     if (collection) {
-      if (doingSearch) {
+      if (criteria.some(({ value }) => value)) {
         return collection.filter(({
           name, sex, fur_length, declawed, date_of_birth,
           dominant_breed: { name: breed }, animal_color: { name: color },
           good_with_children, good_with_cats, good_with_dogs,
-        }) => name.toUpperCase().includes((searchName || '').toUpperCase())
+        }) => criteria.every() name.toUpperCase().includes((searchName || '').toUpperCase())
           && (!searchGender || sex === searchGender.value)
           && (!searchBreed || breed === searchBreed.value)
           && (!searchColor || color === searchColor.value)
