dbc4ddcc04383d0103d800377f7f63f0c82625a0
Steven Pothoven <steven@pothoven.net>
2020-10-14 21:18:28 -0400

    Use HashRouter instead of BrowserRouter to allow page reloads

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@
 
 import React, { useEffect } from 'react'
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Switch,
   Route,
   useLocation,
@@ -49,7 +49,7 @@ const ScrollToTop = () => {
 }
 
 const App = () => (
-  <Router basename="/">
+  <Router basename="/" >
     <ScrollToTop />
     <GlobalDataProvider>
       <Switch>
