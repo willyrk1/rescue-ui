6bb2333c56e6199140d8bf88091ec2da6d745306
Steven Pothoven <steven@pothoven.net>
2021-10-27 17:46:08 -0400

    Switch to Matomo package to fix CORS

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import {
   useHistory,
 } from 'react-router-dom'
 import ReactGA from 'react-ga'
+import { useMatomo } from '@datapunt/matomo-tracker-react'
 import { GlobalDataProvider } from './context/GlobalDataContext'
 import Home from './components/Home/Home'
 import Adopt from './components/Adopt'
@@ -41,16 +42,17 @@ import UnderConstruction from './components/UnderConstruction'
 import { ErrorPage } from './components/Error'
 import './App.scss'
 
-ReactGA.initialize('UA-6912841-1');
-ReactGA.pageview(window.location.pathname);
 
 const ScrollToTop = () => {
   const history = useHistory()
+  const { trackPageView } = useMatomo()
+
   useEffect(() => {
     if (history.action === 'PUSH') {
       window.scrollTo(0, 0);
       ReactGA.set({ page: history.location.pathname });
       ReactGA.pageview(history.location.pathname);
+      trackPageView({ href: history.location.pathname});
     }
   }, [history.location, history.action])
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import ReactGA from 'react-ga'
+import { MatomoProvider, createInstance } from '@datapunt/matomo-tracker-react'
 import './assets/stylesheets/_variables.scss'
 import './assets/stylesheets/style.scss'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+// Google Analytics
+ReactGA.initialize('UA-6912841-1');
+ReactGA.pageview(window.location.pathname);
+
+// Matomo
+const matomoInstance = createInstance({
+  urlBase: 'https://analytics.pothoven.net/',
+  siteId: 8,
+  linkTracking: false,
+})
+
+ReactDOM.render(
+  <MatomoProvider value={matomoInstance}>
+    <App />
+  </MatomoProvider>,
+  document.getElementById('root')
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
