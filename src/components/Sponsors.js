91b5e2e33a6152a0dda275bfbe807c45476d78eb
willyrk1 <willyrk1@gmail.com>
2022-03-09 23:53:00 -0500

    Remove two sponsors.

--- src/components/Sponsors.js
+++ src/components/Sponsors.js
@@ -2,9 +2,7 @@ import React from 'react'
 import classNames from 'classnames/bind'
 import Scroller from './Scroller'
 import styles from './Sponsors.module.scss'
-import bigCatLogo from '../assets/images/BigCatRescueLogo.png'
 import petcoLogo from '../assets/images/PetcoLogo.png'
-import gulfCoastLogo from '../assets/images/Gulfcoast Veterinary Center.png'
 import bisselLogo from '../assets/images/bisselpetfoundation.jpg'
 import petSmartLogo from '../assets/images/petsmart_charities-1.jpg'
 import elseyLogo from '../assets/images/DrElseysLogo_2021.jpg'
@@ -25,22 +23,12 @@ const Sponsors = () => {
             { key: 8, component: <img src={powasnickLogo} alt="Powasnick" /> },
             { key: 1, component: <img src={petcoLogo} alt="Petco" /> },
             {
-              key: 2,
-              component: (
-                <img src={gulfCoastLogo} alt="Gulf Coast Veterinary" />
-              ),
-            },
-            {
               key: 4,
               component: (
                 <a href="https://petsmartcharities.org/">
                   <img src={petSmartLogo} alt="PetSmart" />
                 </a>
               )
-            },
-            {
-              key: 5,
-              component: <img src={bigCatLogo} alt="Big Cat Rescue" />,
             },
             {
               key: 6,
